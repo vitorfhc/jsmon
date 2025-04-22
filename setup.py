6d4758f66a529f91774b653825867bc421b42215
Vitor Falcao <vitorfhc@protonmail.com>
2025-04-22 17:24:33 -0300

    Update setup.py to enhance package metadata and dependencies
    - Changed string quotes from single to double for consistency.
    - Added 'html5lib' and 'beautifulsoup4' to the install_requires list to support HTML content handling.

--- setup.py
+++ setup.py
@@ -2,14 +2,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='JSMon',
+    name="JSMon",
     packages=find_packages(),
-    version='1.0',
+    version="1.0",
     description="A python script that monitors JavaScript files.",
-    long_description=open('README.md').read(),
-    author='r0bre',
-    author_email='mail@r0b.re',
-    license='MIT',
-    url='https://github.com/robre/jsmon',
-    install_requires=['requests', 'jsbeautifier', 'python-decouple','slackclient'],
+    long_description=open("README.md").read(),
+    author="r0bre",
+    author_email="mail@r0b.re",
+    license="MIT",
+    url="https://github.com/robre/jsmon",
+    install_requires=[
+        "requests",
+        "jsbeautifier",
+        "python-decouple",
+        "html5lib",
+        "beautifulsoup4",
+    ],
 )
